8afcf076927cda32ed66c00a7d7e95f45d80ca5c
TMatzCUW <70924911+TMatzCUW@users.noreply.github.com>
2020-11-24 11:01:10 -0600

    I start back

--- app/src/main/java/com/example/diceroller/MainActivity.java
+++ app/src/main/java/com/example/diceroller/MainActivity.java
@@ -3,8 +3,29 @@ package com.example.diceroller;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    private Button button1;
+    private Button button2;
+    private Button button3;
+    private Button button4;
+    private Button button5;
+    private Button button6;
+    private Button button7;
+    private Button button8;
+    private Button button9;
+    private Button button0;
+    private Button buttonPercentile;
+    private Button buttonRoll;
+    private Button buttonD4;
+    private Button buttonD6;
+    private Button buttonD8;
+    private Button buttonD10;
+    private Button buttonD12;
+    private Button buttonD20;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
